270cd3ae6946cff0ebac1c74b0df06d0be936927
KhallilB <khallil.bailey@gmail.com>
2018-12-04 17:47:20 -0800

    SERVER CONFIGS

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+//DEPENDENCIES
 const express = require("express");
 const cors = require("cors");
+const upload = require("./upload");
+
+//ALLOWS ANY DOMAIN
 var corsOptions = {
-  origin: "http://localhost:4200",
+  origin: "*",
   optionsSuccessStatus: 200
 };
 
 const server = express();
 server.use(cors(corsOptions));
 
+//ROUTES
+server.post("/upload", upload);
+
+//LISTENING
 port = process.env.Port || 4000;
 server.listen(port, () => {
   console.log("Server Working!");
